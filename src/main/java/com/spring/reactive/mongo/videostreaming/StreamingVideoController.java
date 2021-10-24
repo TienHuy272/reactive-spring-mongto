df18cbe8b79fc1ff7ef198aadb6512c7b1045849
Nguyen Tien Huy <tienhuy@Nguyens-MacBook-Pro.local>
2021-10-24 15:42:06 +0700

    demo reactive with video

---
+++ src/main/java/com/spring/reactive/mongo/videostreaming/StreamingVideoController.java
@@ -0,0 +1,16 @@
+package com.spring.reactive.mongo.videostreaming;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class StreamingVideoController {
+
+
+
+}
